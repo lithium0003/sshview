283ff7aa151bdab13b373db9c9ef3d22f318a939
lithium03 <contact@lithium03.info>
2022-04-21 03:27:51 +0900

    initial file

---
+++ sshview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sshview
+//
+//  Created by rei8 on 2022/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sshview/sshviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sshviewApp.swift
+//  sshview
+//
+//  Created by rei8 on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct sshviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
